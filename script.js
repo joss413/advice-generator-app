f3c19dbbfd3890b26f63fb527b61efa62870e0c4
Yoseph Negash <yosphn22@gmail.com>
2022-12-01 16:48:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateAdvice(id) {
+    console.log(id)
+}
+
+const diceBtn = document.querySelector('.dice');
+
+const diceId = document.getElementById("dice");
+
+
+diceBtn.addEventListener('click', (e)=>{
+  
+})
+
+const diceFunction = () =>{
+
+}
